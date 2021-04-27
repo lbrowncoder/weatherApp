226909fb138d344598ee46e087a2a065a6fc3f90
marab <69154212+lbrowncoder@users.noreply.github.com>
2021-04-27 11:23:47 +0100

    added api

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-window.addEventListener('load', () => {
-let long;
-let lat;
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(position => {
-        long = position.coords.longitude;
-        lat = position.coords.latitude;
-        const api =`'https://api.tomorrow.io/v4/timelines?location=${lat}.${long}&fields=temperature&timesteps=1h&units=metric&apikey=tuHJ7rFWz5N6CGroXJj8TAddMnY1tdWD'`
-        fetch(api)
-        .then(response =>{
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-        })
-    });
-
-}
-});
+const api = {
+    key: "5183bb739ea5d9fbbc0c733b5c4aa9d6",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+  
+  const searchbox = document.querySelector('.search-box');
+  searchbox.addEventListener('keypress', setQuery);
+  
+  function setQuery(evt) {
+    if (evt.keyCode == 13) {
+      getResults(searchbox.value);
+    }
+  }
+  
+  function getResults (query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(weather => {
+        return weather.json();
+      }).then(displayResults);
+  }
+  
+  function displayResults (weather) {
+    console.log(weather);
+  }
 
